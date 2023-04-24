01f2f08d028df9aa798c348d853113c0de4d8c56
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-04-24 10:56:35 +0530

    setstate method in class components

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 // import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
-import MessageClass from './components/MessageClass';
-import MessageStateHook from './components/MessageStateHook';
+// import MessageClass from './components/MessageClass';
+// import MessageStateHook from './components/MessageStateHook';
+import CounterClass from './components/CounterClass';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React I'm Software Developer!</h1>
-      <MessageClass/>
-      <MessageStateHook/>
+      <CounterClass/>
+      {/* <MessageClass/>
+      <MessageStateHook/> */}
       {/* <Greet name="Bruce" heroName="Batman">
         <p>This is a props children</p>
       </Greet>
