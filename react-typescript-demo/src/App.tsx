8386cffb8a1ea97fc67ea374e0fbc384f2f443f6
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-05-02 17:38:12 +0530

    use state is done

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,41 +1,41 @@
 import './App.css';
-import { Button } from './components/Event/Button';
-import { Input } from './components/Event/Input';
-import { Container } from './components/Props/Container';
-import { Heading } from './components/Props/Heading';
-import { Oscar } from './components/Props/Oscar';
-import { Greet } from './components/Props/Greet';
-import { Person } from './components/Props/Person';
-import { PersonList } from './components/Props/PersonList';
-import { Status } from './components/Props/Status';
+// import { Button } from './components/Event/Button';
+// import { Input } from './components/Event/Input';
+// import { Container } from './components/Props/Container';
+// import { Heading } from './components/Props/Heading';
+// import { Oscar } from './components/Props/Oscar';
+// import { Greet } from './components/Props/Greet';
+// import { Person } from './components/Props/Person';
+// import { PersonList } from './components/Props/PersonList';
+// import { Status } from './components/Props/Status';
+import { User } from './components/UseState/User';
 
 function App() {
-  const PersonName = {
-    first : "Vivek",
-    last : "Patel"
-  }
-  const nameList = [
-    {
-      first : "Bruce",
-      last : "Wayne"
-    },
-    {
-      first : "Clark",
-      last : "Kent"
-    },
-    {
-      first : "Princess",
-      last : "Diana"
-    }
-  ]
+  // const PersonName = {
+  //   first : "Vivek",
+  //   last : "Patel"
+  // }
+  // const nameList = [
+  //   {
+  //     first : "Bruce",
+  //     last : "Wayne"
+  //   },
+  //   {
+  //     first : "Clark",
+  //     last : "Kent"
+  //   },
+  //   {
+  //     first : "Princess",
+  //     last : "Diana"
+  //   }
+  // ]
 
   return (
     <div className="App">
-      <Greet name="Vivek" messageCount={10} isLoggedIn = {true}/>
-
+      {/* props */}
+      {/* <Greet name="Vivek" messageCount={10} isLoggedIn = {true}/> */}
       {/* optional method */}
-      <Greet name="Vivek"  isLoggedIn = {true}/>
-
+      {/* <Greet name="Vivek"  isLoggedIn = {true}/>
       <Person name={PersonName}/>
       <PersonList names={nameList}/>
       <Status status='error'/>
@@ -49,7 +49,10 @@ function App() {
       }}/>
 
       <Input value='' handleChange={(event)=>console.log(event)}/>
-      <Container styles={{border:'1px solid black',padding:'1rem'}}/>
+      <Container styles={{border:'1px solid black',padding:'1rem'}}/> */}
+
+      {/* useState */}
+      <User/>
     </div>
   );
 }
